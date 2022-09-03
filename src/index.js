d8b2f5517fecce1f8e509d2574bc8f87861b1797
edijavi <edilata@msn.com>
2022-09-03 13:17:24 +0200

    boilerplate

---
+++ src/index.js
@@ -0,0 +1,7 @@
+document.getElementById("app").innerHTML = `
+<h1>Hello Team!</h1>
+<div>
+  This is my assigment
+  <a href="https://github.com/edijavi" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
